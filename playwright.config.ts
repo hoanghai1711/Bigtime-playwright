70e1c8bf0560064832ad43fde2fcfbed6dcd3796
Hoang Minh Hai <17112004hai@gmail.com>
2026-01-14 08:31:07 +0700

    run ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,16 +21,16 @@ export default defineConfig({
 
   use: {
     
-//     headless: false, // true run in headless mode. false run with browser window opened.
-//     viewport: null,
-// screenshot: 'only-on-failure',
-//   video: 'retain-on-failure',
-//   trace: 'on-first-retry',  
+    headless: false, // true run in headless mode. false run with browser window opened.
+    viewport: null,
+screenshot: 'only-on-failure',
+  video: 'retain-on-failure',
+  trace: 'on-first-retry',  
 
-    headless: true, // 🔥 BẮT BUỘC CHO CI
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    trace: 'on-first-retry',
+    // headless: true, // 🔥 BẮT BUỘC CHO CI
+    // screenshot: 'only-on-failure',
+    // video: 'retain-on-failure',
+    // trace: 'on-first-retry',
     actionTimeout: 10000,
       launchOptions: {
       args: ['--start-maximized',],
